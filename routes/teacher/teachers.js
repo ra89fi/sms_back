01083c6aa6e43ad03b04a3286137e7f3cb250c31
raihan <r_firoz@bitbucket.org>
2019-08-24 14:46:20 +0600

    teacher list and delete route

--- routes/teacher/teachers.js
+++ routes/teacher/teachers.js
@@ -50,4 +50,28 @@ router.post("/", (req, res) => {
   );
 });
 
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM sms_teachers", (error, results, fields) => {
+    if (error) {
+      console.log(error.message);
+      return res.status(500).send("ERROR");
+    }
+    console.log("results length", results.length);
+    res.json(results);
+  });
+});
+
+router.post("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  if (!id) return res.status(400).send("ERROR");
+  db.query("DELETE FROM sms_teachers WHERE id=?", req.params.id, (error, results, fields) => {
+    if (error) {
+      console.log(error.message);
+      return res.status(500).send("ERROR");
+    }
+    console.log(results);
+    res.send("OK");
+  });
+});
+
 module.exports = router;
